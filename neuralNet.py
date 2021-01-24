d5f1b556cc384676c7de4bf3665a89f3a9301de0
Sean <sean.s.anastasi@gmail.com>
2021-01-24 02:29:43 +0100

    Add epoch to bad facts graph

--- neuralNet.py
+++ neuralNet.py
@@ -2,6 +2,7 @@ import xlrd
 from numpy import *
 import random
 import time
+from matplotlib import pyplot as plt
 
 input_matrix, result_matrix = (None for i in range(2))
 
@@ -42,7 +43,7 @@ def sampleData(number):
     return random.sample(arr,number)
 
 class NeuralNetwork:
-    input_nodes, hidden_nodes, output_nodes,learning_rate = (None for i in range(4))
+    input_nodes, hidden_nodes, output_nodes,learning_rate, error_threshold = (None for i in range(5))
     weights_ih, weights_ho, bias_h, bias_o, hidden, inputs = ([] for i in range(6))
     def __init__(self, nInput, nHidden, nOutput):
 
@@ -54,6 +55,7 @@ class NeuralNetwork:
         self.bias_h = self.RandomiseWeights(self.hidden_nodes, 1 , self.bias_h )
         self.bias_o = self.RandomiseWeights(self.output_nodes,  1 , self.bias_o )
         self.learning_rate = 0.2
+        self.error_threshold = 0.2
         
         
         
@@ -92,6 +94,18 @@ class NeuralNetwork:
         outputs = self.FeedForward(inputs, False)
         output_errors = targets - outputs
         
+        good_fact = True
+        
+        for i in output_errors.T:
+            if good_fact == False:
+                good_fact = False
+            else:
+                
+                if not self.error_threshold > float(i) > (self.error_threshold * -1):
+                    good_fact = False
+        
+        if good_fact:
+            return True   
 
         output_gradient = getGradient(outputs)
         output_gradient = multiply(output_gradient, output_errors)
@@ -111,29 +125,49 @@ class NeuralNetwork:
         self.weights_ih = self.weights_ih + weight_ih_deltas
         self.bias_h = self.bias_h + hidden_gradient
         
-        return
+        return False
 
 def main():
     start_time = time.time()
     start()
     nueral_net = NeuralNetwork(5,4,3)
     sample = sampleData(26)
-    
-    
-    
+    bad_facts = []
+    epochs = 0
+    acceptError = False
 
-    for i in range(1000):
-        num = random.choice(sample)
-        
-        nueral_net.train(input_matrix[num], result_matrix[num])
-        
-        
+    while acceptError == False:
+        acceptError = True
+        bad_fact_epoch = 0
+        for j in sample:
+            is_not_error = nueral_net.train(input_matrix[j], result_matrix[j])
+            
+            
+            if acceptError == False:
+                acceptError = False
+            else:
+                acceptError = is_not_error
+
+            if not is_not_error:
+                bad_fact_epoch +=1
+        epochs += 1
+        bad_facts.append(bad_fact_epoch)
+    print(epochs)
+    print(bad_facts)
+
+    epoch_array = []
+    for i in range(epochs):
+        epoch_array.append(i)
+
+    plt.plot(epoch_array, bad_facts)
+    plt.xlabel("Epochs")
+    plt.ylabel("Bad Facts")
     for i  in range(len(input_matrix)):
         
-        
-        print(str(input_matrix[i])+" - "+ str(nueral_net.FeedForward(input_matrix[i], True)))
+        if i not in sample:
+            print(str(input_matrix[i])+" - "+ str(nueral_net.FeedForward(input_matrix[i], False)))
     
-
+    plt.show()
     
     print("--- %s seconds ---" % (time.time() - start_time))
 
